c55fe59c028cb07e4dd0a1f8d56db4db59579d9e
Johnson Kaho Poon <kahopoon@gmail.com>
2016-03-14 17:20:14 +0800

    Initial Commit

---
+++ Alphacamp_AutoLayout_homework/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Alphacamp_AutoLayout_homework
+//
+//  Created by Ka Ho on 14/3/2016.
+//  Copyright © 2016 Ka Ho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
